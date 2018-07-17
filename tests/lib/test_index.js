9e5277e6df85c3569bf6d36e66401a2d8be305b5
William Durand <will+git@drnd.me>
2018-07-17 13:52:06 +0200

    Be node 6 compatible

---
+++ tests/lib/test_index.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const plugin = require('../../lib');
+
+describe('plugin', () => {
+  it('exports all rules', () => {
+    assert.ok(typeof plugin.rules === 'object');
+  });
+
+  it('exports a set of configurations', () => {
+    assert.ok(typeof plugin.configs === 'object');
+  });
+
+  it('provides a "recommended" configuration', () => {
+    assert.ok(typeof plugin.configs.recommended === 'object');
+    assert.deepEqual(plugin.configs.recommended.plugins, ['amo']);
+    assert.ok(typeof plugin.configs.recommended.rules === 'object');
+  });
+});
